0917c042e8480d6bf131c18683fdbd3da5a02611
Sara Noor <saranoor@Saras-Mac.local>
2025-05-13 11:39:08 +0500

    use individual API view method for each endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import AdAPI
+# from .views import AdAPI
+
+# urlpatterns = [
+#     path('', AdAPI.as_view()),
+# ]
+
+from .views import CreateListing, ReadListing, UpdateListing, DeleteListing
 
 urlpatterns = [
-    path('', AdAPI.as_view()),
+    path('create_listing/', CreateListing.as_view(), name='create_listing'),
+    path('read_listing/', ReadListing.as_view(), name='read_listing'),
+    path('update_listing/', UpdateListing.as_view(), name='update_listing'),
+    path('delete_listing/', DeleteListing.as_view(), name='delete_listing'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,28 @@
 from rest_framework import status
 from rest_framework.views import APIView, Response
 
-class AdAPI(APIView):
-    def get(self, request):
-        items = ['apples','mangoes','banana']
-        response_data = {"data": items}
-        return Response(response_data,status=status.HTTP_200_OK)
+# class AdAPI(APIView):
+#     def get(self, request):
+#         items = ['apples','mangoes','banana']
+#         response_data = {"data": items}
+#         return Response(response_data,status=status.HTTP_200_OK)
     
-    def post(self,request):
-        response_data = {"response":"Hello Its Post method"}
-        return Response(response_data,status=status.HTTP_200_OK)
+#     def post(self,request):
+#         response_data = {"response":"Hello Its Post method"}
+#         return Response(response_data,status=status.HTTP_200_OK)
+
+class CreateListing(APIView):
+    def post(self, request):
+        return Response({"message": "Listing created"}, status=status.HTTP_201_CREATED)
+
+class ReadListing(APIView):
+    def get(self, request):
+        return Response({"message": "Here are the listings"}, status=status.HTTP_200_OK)
+
+class UpdateListing(APIView):
+    def put(self, request):
+        return Response({"message": "Listing updated"}, status=status.HTTP_200_OK)
+
+class DeleteListing(APIView):
+    def delete(self, request):
+        return Response({"message": "Listing deleted"}, status=status.HTTP_204_NO_CONTENT)
